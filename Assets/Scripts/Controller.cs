bae5fee209c82fbaf38d5b64734dfa222f1d52d2
Inseon Kim <inskim@ucdavis.edu>
2024-03-16 20:21:24 -0700

    Scene Transition Added

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    FadeInOut fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = FindObjectOfType<FadeInOut>();
+
+        fade.FadeOut();
+    }
+}
